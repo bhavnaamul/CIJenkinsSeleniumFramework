4dadea9d0cb4b38f6c65171b5e173f7b1e5eb616
bhavna <bhavna.amul@gmail.com>
2026-05-11 17:30:37 +0530

    new page feature

---
+++ src/test/java/pages/CheckoutPageWithLoggedUser.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPageWithLoggedUser extends BasePage{
+
+	public CheckoutPageWithLoggedUser(WebDriver driver) {
+		super(driver);
+	}
+
+}
